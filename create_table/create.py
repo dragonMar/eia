934d8cec2bc15155750b9acd07aca47bd6f46e85
mayexin <mayexinxin@163.com>
2016-09-24 17:33:43 +0800

    add psql

--- create_table/create.py
+++ create_table/create.py
@@ -23,7 +23,7 @@ engineer_sql="CREATE TABLE engineer(\
 unit_sql = "CREATE TABLE unit\
         (id serial NOT NULL, nid integer  NOT NULL,province character varying(20),\
         city character varying(20), org_name character varying(50), \
-        leve character varying(1), cq_code character varying(20), \
+        level character varying(1), cq_code character varying(20), \
         a_scope character varying(200), b_scope character varying(200), \
         table_scope character varying(10), valid_date date, \
         base_info character varying(500), cop character varying(20),\
@@ -31,8 +31,47 @@ unit_sql = "CREATE TABLE unit\
         create_time date)"
 
 
+air_history_sql = "create table air_history(\
+        id serial NOT NULL, city character varying(20) NOT NULL,\
+        pubtime timestamp, api integer, level character varying(20),\
+        pollution character varying(20), status character varying(20),\
+        create_time timestamp)"
+air_daily_sql = "create table air_daily(\
+        id serial NOT NULL, city character varying(20) NOT NULL,\
+        pubtime timestamp, aqi integer, level character varying(20),\
+        pollution character varying(20))"
+air_hour_sql = "create table air_hour(\
+        id serial NOT NULL, city character varying(20) NOT NULL,\
+        pubtime timestamp, aqi integer, level character varying(20),\
+        pollution character varying(20))"
+epa_area_sql = "create table epa_area(\
+        id serial NOT NULL, code character varying(20),\
+        epa_name character varying(20), area character varying(50),\
+        size integer, objects character varying(50), kinds character varying(20),\
+        level character varying(20), set_date timestamp, chargedby character varying(20),\
+        create_time timestamp)"
+limit_goods_sql =  "create table limit_goods(\
+        id serial NOT NULL, year integer NOT NULL, batch integer,\
+        approve_date timestamp, import_unit character varying(20),\
+        manu_unit character varying(20), goods_code character varying(50), \
+        goods_name character varying(50), approve_amount integer,\
+        approve_port character varying(20),create_time timestamp)"
+auto_goods_sql = "create table auto_goods(\
+        id serial NOT NULL, batch integer NOT NULL, approve_date timestamp,\
+        import_unit character varying(30), manu_unit character varying(30),\
+        goods_code character varying(50), goods_name character varying(20),\
+        approve_amount integer, approve_port character varying(30),\
+        create_time timestamp)"
+
+
 if __name__ =="__main__":
-    conn_psql(person_sql)
-    conn_psql(engineer_sql)
-    conn_psql(unit_sql)
+    #conn_psql(person_sql)
+    #conn_psql(engineer_sql)
+    #conn_psql(unit_sql)
+    conn_psql(air_history_sql)
+    conn_psql(air_daily_sql)
+    conn_psql(air_hour_sql)
+    conn_psql(epa_area_sql)
+    conn_psql(limit_goods_sql)
+    conn_psql(auto_goods_sql)
 
